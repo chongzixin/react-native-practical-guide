db1b3c89dc7fbf7c270bc70e52fb8fe954168cc6
zixin <czixin@gmail.com>
2020-10-10 20:46:38 +0800

    finished start game screen

--- section-4/guess-a-number/screens/StartGameScreen.js
+++ section-4/guess-a-number/screens/StartGameScreen.js
@@ -1,21 +1,80 @@
-import React from 'react';
-import { View, StyleSheet, Text, TextInput, Button } from 'react-native';
+import React, { useState } from 'react';
+import { View, StyleSheet, Text, Button, TouchableWithoutFeedback, Keyboard, Alert } from 'react-native';
 
 import Card from '../components/Card';
+import Input from "../components/Input";
+import NumberContainer from '../components/NumberContainer';
+import Colors from "../constants/colors";
 
 const StartGameScreen = props => {
-    return (
-        <View style={styles.screen}>
-            <Text style={styles.title}>Start a new Game!</Text>
-            <Card style={styles.inputContainer}>
-                <Text>Select a Number</Text>
-                <TextInput />
-                <View style={styles.buttonContainer}>
-                    <Button title="Reset" onPress={() => {}} />
-                    <Button title="Confirm" onPress={() => {}} />
-                </View>
+    const [enteredValue, setEnteredValue] = useState('');
+    const [confirmed, setConfirmed] = useState(false);
+    const [selectedNumber, setSelectedNumber] = useState();
+
+    const numberInputHandler = inputText => {
+        // use a regex to replace all non-numbers away
+        setEnteredValue(inputText.replace(/[^0-9]/g, ''));
+    };
+
+    const resetInputHandler = () => {
+        setEnteredValue('');
+        setConfirmed(false);
+    };
+
+    const confirmInputHandler = () => {
+        const chosenNumber = parseInt(enteredValue);
+        if(isNaN(chosenNumber) || chosenNumber <= 0 || chosenNumber > 99) {
+            Alert.alert('Invalid number',
+                        'Number has to be a number between 1 and 99.',
+                        [{text: 'Okay', style:'destructive',
+                        onPress: resetInputHandler
+                    }])
+            return;
+        }
+        setConfirmed(true);
+        setEnteredValue('');
+        // this can come after setting entered value because these state changes are batched and will only be updated at the next run.
+        setSelectedNumber(chosenNumber);
+        Keyboard.dismiss();
+    };
+
+    let confirmedOutput;
+    if(confirmed) {
+        confirmedOutput  = (
+            <Card style={styles.summaryContainer}>
+                <Text>You selected</Text>
+                <NumberContainer>{selectedNumber}</NumberContainer>
+                <Button title="START GAME" />
             </Card>
-        </View>
+        )
+
+        // style this, and include a button
+    }
+
+    return (
+        <TouchableWithoutFeedback onPress={() => {Keyboard.dismiss()}}>
+            <View style={styles.screen}>
+                <Text style={styles.title}>Start a new Game!</Text>
+                <Card style={styles.inputContainer}>
+                    <Text>Select a Number</Text>
+                    <Input
+                        blurOnSubmit
+                        autoCapitalize='none'
+                        autoCorrect={false}
+                        keyboardType='number-pad'
+                        maxLength={2}
+                        style={styles.input}
+                        value={enteredValue}
+                        onChangeText={numberInputHandler}
+                    />
+                    <View style={styles.buttonContainer}>
+                        <View style="styles.button"><Button color={Colors.accent} title="Reset" onPress={resetInputHandler} /></View>
+                        <View style="styles.button"><Button color={Colors.primary} title="Confirm" onPress={confirmInputHandler} /></View>
+                    </View>
+                </Card>
+                {confirmedOutput}
+            </View>
+        </TouchableWithoutFeedback>
     );
 };
 
@@ -39,6 +98,17 @@ const styles = StyleSheet.create({
         width: '100%',
         justifyContent: 'space-between',
         paddingHorizontal: 15,
+    },
+    button: {
+        width:"40%",
+    },
+    input: {
+        width: 50,
+        textAlign: "center",
+    },
+    summaryContainer: {
+        margin: 20,
+        alignItems: "center"
     }
 });
 
