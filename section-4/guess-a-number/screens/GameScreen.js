60918a6e63f435862a91fa4f36f9cd5afe71e919
chongzixin <zixin@chope.co>
2020-10-10 23:50:56 +0800

    finished game logic

--- section-4/guess-a-number/screens/GameScreen.js
+++ section-4/guess-a-number/screens/GameScreen.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import React, { useState, useRef, useEffect } from 'react';
+import { View, Text, StyleSheet, Button, Alert } from 'react-native';
 
 import NumberContainer from '../components/NumberContainer';
 import Card from '../components/Card';
@@ -8,7 +8,7 @@ import Card from '../components/Card';
 const generateRandomBetween = (min, max, exclude) => {
     min = Math.ceil(min);
     max = Math.floor(max);
-    const randomNum = (Math.random() * (max-min)) + min;
+    const randomNum = Math.floor((Math.random() * (max-min)) + min);
     if (randomNum === exclude) {
         return generateRandomBetween(min, max, exclude);
     } else {
@@ -18,14 +18,48 @@ const generateRandomBetween = (min, max, exclude) => {
 
 const GameScreen = props => {
     const [currentGuess, setCurrentGuess] = useState(generateRandomBetween(1,100, props.userChoice));
+    const [rounds, setRounds] = useState(0);
+    const currentLow = useRef(1);
+    const currentHigh = useRef(100);
+
+    // useEffect takes a function that is run after every render cycle
+    // the second parameter takes an array of items that we want to watch for changes. so if it doesn't change then the effect wont run.
+    // in this case our dependencies are currentGuess, userChoice and onGameOver, so we use object destructuring
+    const { userChoice, onGameOver } = props;
+    useEffect(() => {
+        if(currentGuess === userChoice) {
+            onGameOver(rounds);
+        }
+    }, [currentGuess, userChoice, onGameOver]);
+
+    const nextGuessHandler = direction => {
+        // first check if the user gave the wrong hint
+        if ( 
+            (direction === 'lower' && currentGuess < props.userChoice) || 
+            (direction === 'greater' && currentGuess > props.userChoice) 
+        ) {
+                Alert.alert("Don't lie!", "You know that this is wrong.", [{text: "Sorry!", style: 'cancel'}]);
+                return;
+        }
+
+        // handle the hints and return another guess
+        if(direction === 'lower') {
+            currentHigh.current = currentGuess;
+        } else {
+            currentLow.current = currentGuess;
+        }
+        const nextNumber = generateRandomBetween(currentLow.current, currentHigh.current, currentGuess);
+        setCurrentGuess(nextNumber);
+        setRounds(currentRounds => currentRounds + 1);
+    }
 
     return (
         <View style={styles.screen}>
             <Text>Opponent's Guess</Text>
             <NumberContainer>{currentGuess}</NumberContainer>
             <Card style={styles.buttonContainer}>
-                <Button title="LOWER" onPress={() => {}}/>
-                <Button title="GREATER" onPress={() => {}}/>
+                <Button title="LOWER" onPress={nextGuessHandler.bind(this, 'lower')}/>
+                <Button title="GREATER" onPress={nextGuessHandler.bind(this, 'greater')}/>
             </Card>
         </View>
     )
