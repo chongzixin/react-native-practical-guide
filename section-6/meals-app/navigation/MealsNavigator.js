2787dbcc5b0e3ea2da6e5cfde02cbc79c916f870
chongzixin <3539074+chongzixin@users.noreply.github.com>
2020-10-14 22:44:39 +0800

    got tabs working and changed some style

--- section-6/meals-app/navigation/MealsNavigator.js
+++ section-6/meals-app/navigation/MealsNavigator.js
@@ -1,10 +1,14 @@
+import React from 'react';
 import { Platform } from 'react-native';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+import { Ionicons } from '@expo/vector-icons';
 
 import CategoriesScreen from '../screens/CategoriesScreen';
 import CategoryMealScreens from '../screens/CategoryMealsScreen'
 import MealDetailScreen from '../screens/MealDetailScreen';
+import FavouritesScreen from '../screens/FavouritesScreen';
 
 import Colors from '../constants/Colors';
 
@@ -23,4 +27,25 @@ const MealsNavigator = createStackNavigator({
     }
 });
 
-export default createAppContainer(MealsNavigator);
+const MealsFavTabNavigator = createBottomTabNavigator({
+    Meals: { 
+        screen: MealsNavigator, 
+        navigationOptions: {
+            tabBarIcon: (tabInfo) => {
+                return <Ionicons name='ios-restaurant' size={25} color={tabInfo.tintColor}/>
+            }
+    }},
+    Favourites: { 
+        screen: FavouritesScreen, 
+        navigationOptions: {
+            tabBarIcon: (tabInfo) => {
+                return <Ionicons name='ios-star' size={25} color={tabInfo.tintColor}/>
+            }
+    }},
+}, {
+    tabBarOptions: {
+        activeTintColor: Colors.accentColor,
+    }
+});
+
+export default createAppContainer(MealsFavTabNavigator);
