961b9c4c777164cf6dcb7992a451a87768a3812d
zixin <czixin@gmail.com>
2020-10-14 09:36:36 +0800

    added navigation options like header title etc

--- section-6/meals-app/screens/CategoriesScreen.js
+++ section-6/meals-app/screens/CategoriesScreen.js
@@ -2,17 +2,46 @@
 // grid view that's the home screen
 
 import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import { View, Text, FlatList, StyleSheet, TouchableOpacity, Platform } from 'react-native';
+
+import { CATEGORIES } from '../data/dummy-data';
+import Colors from '../constants/Colors';
 
 const CategoriesScreen = props => {
+    const renderGridItem = (itemData) => {
+        return (
+            <TouchableOpacity
+                style={styles.gridItem}
+                onPress={() => {
+                    props.navigation.navigate({
+                        routeName:'CategoryMeals',
+                        params: {
+                            categoryId: itemData.item.id
+                        }
+                    });
+                }}>
+                <View>
+                    <Text>{itemData.item.title}</Text>
+                </View>
+            </TouchableOpacity>
+        );
+    };
+
     return (
-        <View style={styles.screen}>
-            <Text>The Categories Screen!</Text>
-            <Button title="Go to Meals" onPress={() => {
-                props.navigation.navigate({routeName: 'CategoryMeals'});
-            }} />
-        </View>
+        <FlatList
+            numColumns={2}
+            data={CATEGORIES}
+            renderItem={renderGridItem}
+        />
     );
+};
+
+CategoriesScreen.navigationOptions = {
+    headerTitle: 'Meal Categories',
+    headerStyle: {
+        backgroundColor: Platform.OS === 'android' ? Colors.primaryColor : '',
+    },
+    headerTintColor: Platform.OS === 'android' ? 'white': Colors.primaryColor,
 };
 
 const styles = StyleSheet.create({
@@ -20,7 +49,12 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
-    }
+    },
+    gridItem: {
+        flex: 1,
+        margin: 15,
+        height: 150,
+    },
 });
 
 export default CategoriesScreen;
--- section-6/meals-app/screens/CategoryMealsScreen.js
+++ section-6/meals-app/screens/CategoryMealsScreen.js
@@ -1,7 +1,9 @@
 // page that shows meals for chosen category.
 
 import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import { View, Text, StyleSheet, Button, Platform } from 'react-native';
+
+import { CATEGORIES } from '../data/dummy-data';
 
 const CategoryMealsScreen = props => {
     return (
@@ -13,6 +15,19 @@ const CategoryMealsScreen = props => {
         </View>
     );
 };
+
+CategoryMealsScreen.navigationOptions = navigationData => {
+    const catId = navigationData.navigation.getParam('categoryId');
+    const selectedCategory = CATEGORIES.find(cat => cat.id === catId);
+
+    return {
+        headerTitle: selectedCategory.title,
+        headerStyle: {
+            backgroundColor: Platform.OS === 'android' ? Colors.primaryColor : '',
+        },
+        headerTintColor: Platform.OS === 'android' ? 'white': Colors.primaryColor,
+    };
+}
 
 const styles = StyleSheet.create({
     screen: {
