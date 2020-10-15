033d02034cb23149957a206ff668e695ca16fa2a
Chong Zixin <chongzixin@chongs-mbp.lan>
2020-10-15 11:10:50 +0800

    passed filter data from component to navigation option using useEffect

--- section-6/meals-app/screens/FiltersScreen.js
+++ section-6/meals-app/screens/FiltersScreen.js
@@ -1,15 +1,58 @@
 // filters like gluten free, vegan etc.
 
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState, useEffect, useCallback } from 'react';
+import { View, Text, StyleSheet, Switch, Platform } from 'react-native';
 
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
+import DefaultText from '../components/DefaultText';
 import HeaderButton from '../components/HeaderButton';
 
+import Colors from '../constants/Colors';
+
+const FilterSwitch = props => {
+    return (
+        <View style={styles.filterContainer}>
+            <DefaultText>{props.label}</DefaultText>
+            <Switch 
+                value={props.state}
+                onValueChange={props.onChange}
+                trackColor={{true: Colors.primaryColor}}
+                thumbColor={Platform.OS === 'android' ? Colors.primaryColor : ''}
+            />
+        </View>
+    );
+};
+
 const FiltersScreen = props => {
+    const { navigation } = props;
+    const [isGlutenFree, setIsGlutenFree] = useState(false);
+    const [isVegan, setIsVegan] = useState(false);
+    const [isVegetarian, setIsVegetarian] = useState(false);
+    const [isLactoseFree, setIsLactoseFree] = useState(false);
+
+    // function that gathers all the current filter settings so that it can be passed to the navigation option via useEffect
+    const saveFilters = useCallback(() => {
+        const appliedFilters = {
+            glutenFree: isGlutenFree,
+            vegan: isVegan,
+            vegetarian: isVegetarian,
+            lactoseFree: isLactoseFree,
+        }
+
+        console.log(appliedFilters);
+    }, [isGlutenFree, isVegetarian, isVegetarian, isLactoseFree]);
+
+    useEffect(() => {
+        navigation.setParams({save: saveFilters});
+    }, [saveFilters]);
+
     return (
         <View style={styles.screen}>
-            <Text>The Filters Screen!</Text>
+            <DefaultText style={styles.title}>Available Filters / Restrictions</DefaultText>
+            <FilterSwitch label="Gluten-free" state={isGlutenFree} onChange={newValue => setIsGlutenFree(newValue)} />
+            <FilterSwitch label="Vegan" state={isVegan} onChange={newValue => setIsVegan(newValue)} />
+            <FilterSwitch label="Vegetarian" state={isVegetarian} onChange={newValue => setIsVegetarian(newValue)} />
+            <FilterSwitch label="Lactose-free" state={isLactoseFree} onChange={newValue => setIsLactoseFree(newValue)} />
         </View>
     );
 };
@@ -23,15 +66,32 @@ FiltersScreen.navigationOptions = navData => {
                     navData.navigation.toggleDrawer();
                 }} />
             </HeaderButtons>
-        )
+        ),
+        headerRight: (
+            <HeaderButtons HeaderButtonComponent={HeaderButton}>
+                <Item title="Save" iconName='ios-save' onPress={navData.navigation.getParam('save')} />
+            </HeaderButtons>
+        ),
     };
 };
 
 const styles = StyleSheet.create({
     screen: {
         flex: 1,
-        justifyContent: 'center',
         alignItems: 'center',
+    },
+    title: {
+        fontFamily: 'open-sans-bold',
+        fontSize: 22,
+        margin: 20,
+        textAlign: "center",
+    },
+    filterContainer: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        width: "80%",
+        marginVertical: 15
     }
 });
 
--- section-6/meals-app/screens/MealDetailScreen.js
+++ section-6/meals-app/screens/MealDetailScreen.js
@@ -1,21 +1,40 @@
 // screen that shows details of selected meal like ingredients and recipe
 
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { ScrollView, View, Text, StyleSheet, Image } from 'react-native';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 
 import { MEALS } from '../data/dummy-data';
 import HeaderButton from '../components/HeaderButton';
+import DefaultText from '../components/DefaultText';
+import Colors from '../constants/Colors';
+
+const ListItem = props => {
+    return (
+        <View style={styles.listItem}>
+            <DefaultText>{props.children}</DefaultText>
+        </View>
+    );
+};
 
 const MealDetailScreen = props => {
     const mealId = props.navigation.getParam('mealId');
     const selectedMeal = MEALS.find(meal => meal.id === mealId);
     
     return (
-        <View style={styles.screen}>
-            <Text>{selectedMeal.title}</Text>
-            <Text>The Meal Details Screen!</Text>
-        </View>
+        <ScrollView>
+            <Image style={styles.image} source={{uri: selectedMeal.imageUrl}} />
+            <View style={styles.details}>
+                <DefaultText>{selectedMeal.duration}m</DefaultText>
+                <DefaultText>{selectedMeal.complexity}</DefaultText>
+                <DefaultText>{selectedMeal.affordability}</DefaultText>
+            </View>
+            <DefaultText style={styles.title}>Ingredients</DefaultText>
+            {selectedMeal.ingredients.map(ingredient => <ListItem key={ingredient}>{ingredient}</ListItem>)}
+
+            <DefaultText style={styles.title}>Steps</DefaultText>
+            {selectedMeal.steps.map(step => <ListItem key={step}>{step}</ListItem>)}
+        </ScrollView>
     );
 };
 
@@ -40,10 +59,27 @@ MealDetailScreen.navigationOptions = navigationData => {
 };
 
 const styles = StyleSheet.create({
-    screen: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
+    image: {
+        width: "100%",
+        height: 200,
+    },
+    details: {
+        flexDirection: 'row',
+        padding: 15,
+        justifyContent: "space-around",
+    },
+    title: {
+        fontFamily: 'open-sans-bold',
+        fontSize: 22,
+        textAlign: "center",
+        color: Colors.accentColor,
+    },
+    listItem: {
+        marginVertical: 10,
+        marginHorizontal: 20,
+        borderColor: '#ccc',
+        borderWidth: 1,
+        padding: 10,
     }
 });
 
