dcc832ac5e4853bf0ce4013ce6fe3b2261d32146
chongzixin <3539074+chongzixin@users.noreply.github.com>
2020-10-17 20:54:02 +0800

    add new places and added redux

--- section-12/native-features-app/App.js
+++ section-12/native-features-app/App.js
@@ -1,16 +1,19 @@
 import React from 'react';
-import { StyleSheet } from 'react-native';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import ReduxThunk from 'redux-thunk';
+
 import PlacesNavigator from './navigation/PlacesNavigator';
+import placesReducer from './store/places-reducer';
+
+const rootReducer = combineReducers({
+  places:placesReducer
+});
+
+const store = createStore(rootReducer, applyMiddleware(ReduxThunk));
 
 export default function App() {
-  return <PlacesNavigator />;
+  return <Provider store={store}>
+    <PlacesNavigator />
+  </Provider>;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- section-12/native-features-app/screens/NewPlaceScreen.js
+++ section-12/native-features-app/screens/NewPlaceScreen.js
@@ -1,11 +1,40 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { ScrollView, View, Button, Text, TextInput, StyleSheet } from 'react-native';
+import Colors from '../constants/Colors';
+
+import { useDispatch } from 'react-redux';
+import * as placesActions from '../store/places-action';
 
 const NewPlaceScreen = props => {
+    const [titleValue, setTitleValue] = useState('');
+    const dispatch = useDispatch();
+
+    const titleChangeHandler = text => {
+        // can do more validation
+        setTitleValue(text);
+    }
+
+    const savePlaceHandler = () => {
+        dispatch(placesActions.addPlace(titleValue));
+        props.navigation.goBack();
+    };
+
     return (
-        <View>
-            <Text>NewPlaceScreen</Text>
-        </View>
+        <ScrollView>
+            <View style={styles.form}>
+                <Text style={styles.label}>Title</Text>
+                <TextInput 
+                    style={styles.textInput}
+                    onChangeText={titleChangeHandler}
+                    value={titleValue}
+                />
+                <Button 
+                    title="Save Place" 
+                    color={Colors.primary} 
+                    onPress={savePlaceHandler} 
+                />
+            </View>
+        </ScrollView>
     )
 };
 
@@ -14,7 +43,20 @@ NewPlaceScreen.navigationOptions = {
 };
 
 const styles = StyleSheet.create({
-
+    form: {
+        margin: 30
+    },
+    label: {
+        fontSize: 18,
+        marginBottom: 15,
+    },
+    textInput: {
+        borderBottomColor: '#ccc',
+        borderBottomWidth: 1,
+        marginBottom: 15,
+        paddingVertical: 4,
+        paddingHorizontal: 2,
+    }
 });
 
 export default NewPlaceScreen;
