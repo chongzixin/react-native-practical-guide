a3684b077f63dfa5ccb920d71488d4771f797a94
chongzixin <3539074+chongzixin@users.noreply.github.com>
2020-10-19 15:31:00 +0800

    retrieve places from db and load into redux store

--- section-12/native-features-app/helpers/db.js
+++ section-12/native-features-app/helpers/db.js
@@ -16,4 +16,38 @@ export const init = () => {
         });
     });
     return promise;
-}
+};
+
+export const insertPlace = (title, imageUri, address, lat, lng) => {
+    const promise = new Promise((resolve, reject) => {
+        db.transaction((tx) => {
+            tx.executeSql('INSERT INTO places (title, imageUri, address, lat, lng) VALUES (?, ?, ?, ?, ?);', 
+            [title, imageUri, address, lat, lng],
+            (_, result) => {
+                // resolve with result here so that it can be passed back to the calling function
+                resolve(result);
+            },
+            (_, err) => {
+                reject(err);
+            });
+        });
+    });
+    return promise;   
+};
+
+export const fetchPlaces = () => {
+    const promise = new Promise((resolve, reject) => {
+        db.transaction((tx) => {
+            tx.executeSql('SELECT * FROM places;', 
+            [],
+            (_, result) => {
+                // resolve with result here so that it can be passed back to the calling function
+                resolve(result);
+            },
+            (_, err) => {
+                reject(err);
+            });
+        });
+    });
+    return promise;   
+};
--- section-12/native-features-app/store/places-action.js
+++ section-12/native-features-app/store/places-action.js
@@ -1,6 +1,9 @@
 import * as FileSystem from 'expo-file-system';
 
 export const ADD_PLACE = 'ADD_PLACE';
+export const SET_PLACES = 'SET_PLACES';
+
+import { insertPlace, fetchPlaces } from '../helpers/db';
 
 export const addPlace = (title, image) => {
     return async dispatch => {
@@ -12,17 +15,32 @@ export const addPlace = (title, image) => {
                 from: image,
                 to: newPath,
             });
+            const dbResult = await insertPlace(title, newPath, 'Dummy Address', 3.2, 107.1245);
+            console.log(dbResult);
+
+            dispatch({ 
+                type: ADD_PLACE, 
+                placeData: {
+                    id: dbResult.insertId,
+                    title: title,
+                    image: newPath,
+                }
+            });
         } catch(err) {
             console.log(err);
             throw err;
         }
+    };
+};
 
-        dispatch({ 
-            type: ADD_PLACE, 
-            placeData: {
-                title: title,
-                image: newPath,
-            }
-        });
+export const loadPlaces = () => {
+    return async dispatch => {
+        try {
+            const dbResult = await fetchPlaces();
+            console.log(dbResult);
+            dispatch({ type: SET_PLACES, places: dbResult.rows._array });
+        } catch (err) {
+            throw err;
+        }
     };
 };
